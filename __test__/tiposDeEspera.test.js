3b08842c8e912ac97e7ba1836ef63d25d67d4671
Manuel Ricardo <manuelricardohurtado@gmail.com>
2022-04-03 18:43:31 -0400

    Wait For Function

--- __test__/tiposDeEspera.test.js
+++ __test__/tiposDeEspera.test.js
@@ -4,7 +4,8 @@ describe('Tipos de Espera',()=> {
     it('Mostrar todos los diferentes tipos de espera', async() => {
         const browser = await puppeteer.launch({
             headless: false,            
-            defaultViewport: null
+            defaultViewport: null,
+            slowMo: 500
         })
 
         const page = await browser.newPage()
@@ -25,6 +26,18 @@ describe('Tipos de Espera',()=> {
         const button = await page.waitForXPath('//*[@id="showSmallModal"]', {visible: true})
         await button.click()
         
+        //Esperar por una Funcion
+        await page.waitForFunction(() => document.querySelector('#example-modal-sizes-title-sm').innerText === 'Small Modal')
+
+        await page.click('#closeSmallModal')
+        await page.waitForFunction(() => !document.querySelector('#example-modal-sizes-title-sm'))
+
+        //Ejemplo para observar el viewport
+        const observarResize = page.waitForFunction('window.innerWidth < 100')
+        await page.setViewport({width: 50, height:50})
+
+        await observarResize
+
 
         await browser.close()
     }, 350000)
